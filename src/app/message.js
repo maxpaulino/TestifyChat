eec28eae42636ea3b5136d4ef972d48acc1f5e83
Max Paulino <max.paulinor@gmail.com>
2023-06-28 12:32:35 -0600

    Switched back to CommonJS for message.js

--- src/app/message.js
+++ src/app/message.js
@@ -1,23 +1,25 @@
-import { client } from "./src/config/whatsapp";
-import { find } from "./src/app/functions";
-import { runResponse } from "./src/app/response";
+const whatsapp = require("./src/app/whatsapp");
+const functions = require("./src/app/functions");
+const generation = require("./src/app/response");
 
 var history = [];
 
 function callFunction(function_call) {
-  const func = find((func) => func.schema.name === function_call.name);
+  const func = functions.find(
+    (func) => func.schema.name === function_call.name
+  );
   const args = JSON.parse(function_call.arguments);
   return func.function(args);
 }
 
-client.on("message", (message) => {
+whatsapp.client.on("message", (message) => {
   console.log(message.body);
 
   if (message.body.startsWith("?!?")) {
     history.push({ role: "user", content: message.text });
 
     let responseText = "";
-    var response = runResponse(history);
+    var response = generation.runResponse(history);
 
     if (response.function_call) {
       responseText = callFunction(response.function_call);
