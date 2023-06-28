2731a8e24fca5008737310f01ab3b8d9024231fb
Max Paulino <max.paulinor@gmail.com>
2023-06-28 00:06:31 -0600

    Added runQuestionGeneration

--- src/app/response.js
+++ src/app/response.js
@@ -19,4 +19,34 @@ async function runResponse(history) {
   return result.data.choices.shift().message;
 }
 
-module.exports = runResponse();
+async function runQuestionGeneration(tag, level) {
+  var prompt =
+    "Generate a multiple-choice question with 4 options and the answer. " +
+    "This question should be a CEFR certified English Language Exam question at the " +
+    level +
+    " level and should be in relation to this topic: " +
+    tag +
+    ". ";
+
+  const payload = {
+    model: "gpt-3.5-turbo-0613",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are a bot that exclusively generates multiple-choice questions in this format:\n" +
+          "Q:\n\nA)\nB)\nC)\nD)\n\nA:",
+      },
+      { role: "user", content: prompt },
+    ],
+    temperature: 0.75,
+  };
+
+  const result = await openai.createChatCompletion(payload);
+  return result.data.choices.shift().message;
+}
+
+module.exports = {
+  runResponse,
+  runQuestionGeneration,
+};
