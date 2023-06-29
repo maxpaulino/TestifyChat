81af62820d0dc8d996e628d4cc81543b9bc7e524
Max Paulino <max.paulinor@gmail.com>
2023-06-29 12:36:59 -0600

    Fixed runResponse output

--- src/app/runResponse.js
+++ src/app/runResponse.js
@@ -2,7 +2,7 @@ const openai = require("../config/openai.js");
 // const function_schemas = require("./function_schemas.js");
 
 let runResponse = async (history) => {
-  const response = await openai.createChatCompletion({
+  const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo-0613",
     messages: [
       {
@@ -15,7 +15,7 @@ let runResponse = async (history) => {
     // functions: function_schemas.map((func) => func.schema),
   });
 
-  return response.data.choices[0].message.content;
+  return completion.data.choices[0].message.content;
 };
 
 module.exports = runResponse;
