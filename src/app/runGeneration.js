e82bd04b03e761f4eb2474552baedbcdc7c44a17
Max Paulino <max.paulinor@gmail.com>
2023-06-30 12:22:00 -0600

    Formatting

--- src/app/runGeneration.js
+++ src/app/runGeneration.js
@@ -1,13 +1,7 @@
 const openai = require("../config/openai.js");
 
 async function runGeneration(tag, level) {
-  var prompt =
-    "Generate a multiple-choice question with 4 options and the answer. " +
-    "This question should be a CEFR certified English Language Exam question at the " +
-    level +
-    " level and should be in relation to this topic: " +
-    tag +
-    ". ";
+  var prompt = `Generate a multiple-choice question with 4 options and the answer. This question should be a Duolingo English Language question at the ${level} level and should be in relation to this topic: ${tag}. `;
 
   const payload = {
     model: "gpt-3.5-turbo-0613",
