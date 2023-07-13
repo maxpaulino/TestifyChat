f5a60f2a57364705464c0adab8a334ef01b21aaf
Max Paulino <max.paulinor@gmail.com>
2023-07-13 14:35:40 -0600

    Moved response handling to runResponse.js

--- src/app/message.js
+++ src/app/message.js
@@ -4,86 +4,20 @@
 // 'runResponse' is a function used to get a response from the OpenAI API.
 // 'dotenv' is a zero-dependency module that loads environment variables from a .env file into process.env.
 const whatsapp = require("../config/whatsapp.js");
-const function_schemas = require("./function_schemas.js");
 const runResponse = require("./runResponse.js");
 require("dotenv").config();
 
-// Initialize the chat history.
-var history = [];
 // Get the phone number from the environment variables.
 const phoneNumber = process.env.PHONE_NUMBER;
-
-// Define a function 'callFunction' which takes a function call as argument.
-// This function finds the corresponding function schema and runs the function with the given arguments.
-function callFunction(function_call) {
-  // Find the function where the function_schema name is equal to the function_call name.
-  const func = function_schemas.find(
-    (func) => func.schema.name === function_call.name
-  );
-  // Parse the arguments for the function call.
-  const args = JSON.parse(function_call.arguments);
-  // Log the arguments.
-  console.log("Arguments:");
-  console.log(args);
-  // Call the function with the parsed arguments and return the result.
-  return func.function(args);
-}
-
-// Define an asynchronous function 'handlePrompt' which takes a message as argument.
-// This function handles a new message by updating the chat history and getting a response from the OpenAI API.
-async function handlePrompt(message) {
-  // Create a JSON object for the user's message.
-  let messageJSON = {
-    role: "user",
-    content: message,
-  };
-
-  // Add the user's message to the chat history.
-  history.push(messageJSON);
-  // Log the user's message.
-  console.log("User message:");
-  console.log(messageJSON);
-
-  let responseText = "";
-
-  // Get a response from the OpenAI API.
-  let response = await runResponse(history);
-
-  // If the response includes a function call, call the function and add the function's response to the chat history.
-  if (response.function_call) {
-    responseText = await callFunction(response.function_call);
-    let function_response = {
-      role: "function",
-      name: response.function_call.name,
-      content: responseText,
-    };
-    history.push(function_response);
-    console.log("Function response:");
-    console.log(function_response);
-  }
-  // Otherwise, add the assistant's response to the chat history.
-  else {
-    responseText = response.content;
-    let assistant_response = {
-      role: "assistant",
-      content: responseText,
-    };
-    history.push(assistant_response);
-    console.log("Assistant response:");
-    console.log(assistant_response);
-  }
-
-  // Return the text of the response.
-  return responseText;
-}
 
 // Listen for messages from WhatsApp.
 whatsapp.on("message", (message) => {
   // Log the content of the received message.
   console.log(message.body);
   console.log(message.from);
+
   // If the message is from the phone number in the environment variables, handle the prompt and reply with the response.
   if (message.from === phoneNumber) {
-    handlePrompt(message.body).then((response) => message.reply(response));
+    runResponse(message.body).then((response) => message.reply(response));
   }
 });
--- src/app/runResponse.js
+++ src/app/runResponse.js
@@ -4,9 +4,44 @@
 const openai = require("../config/openai.js");
 const function_schemas = require("./function_schemas.js");
 
+// Initialize the chat history.
+var history = [];
+
+
+// Define a function 'callFunction' which takes a function call as argument.
+// This function finds the corresponding function schema and runs the function with the given arguments.
+function callFunction(function_call) {
+  // Find the function where the function_schema name is equal to the function_call name.
+  const func = function_schemas.find(
+    (func) => func.schema.name === function_call.name
+  );
+  // Parse the arguments for the function call.
+  const args = JSON.parse(function_call.arguments);
+  // Log the arguments.
+  console.log("Arguments:");
+  console.log(args);
+  // Call the function with the parsed arguments and return the result.
+  return func.function(args);
+}
+
 // Define an asynchronous function 'runResponse' which takes a chat history as an argument.
 // This function generates a response from the OpenAI API using the provided chat history and a predefined set of function schemas.
-async function runResponse(history) {
+async function runResponse(message) {
+
+  let messageJSON = {
+    role: "user",
+    content: message,
+  };
+  
+  // Add the user's message to the chat history.
+  history.push(messageJSON);
+  // Log the user's message.
+  console.log("User message:");
+  console.log(messageJSON);
+
+  let responseText = "";
+
+
   // Define the payload for the OpenAI API call.
   // The 'model' field specifies the version of the model to use.
   // The 'messages' field contains the conversation history, starting with a system message that sets the behavior of the assistant.
@@ -27,8 +62,33 @@ async function runResponse(history) {
   // Call the OpenAI API to generate a response.
   const completion = await openai.createChatCompletion(payload);
 
+  let response = completion.data.choices[0].message;
+
+  // If the response includes a function call, call the function and add the function's response to the chat history.
+  if (response.function_call) {
+    responseText = await callFunction(response.function_call);
+    let function_response = {
+      role: "function",
+      name: response.function_call.name,
+      content: responseText,
+    };
+    history.push(function_response);
+    console.log("Function response:");
+    console.log(function_response);
+  }
+  // Otherwise, add the assistant's response to the chat history.
+  else {
+    responseText = response.content;
+    let assistant_response = {
+      role: "assistant",
+      content: responseText,
+    };
+    history.push(assistant_response);
+    console.log("Assistant response:");
+    console.log(assistant_response);
+  }
   // Return the content of the first choice from the generated response.
-  return completion.data.choices[0].message;
+  return responseText;
 }
 
 // Export the runResponse function so it can be imported and used in other parts of the application.
